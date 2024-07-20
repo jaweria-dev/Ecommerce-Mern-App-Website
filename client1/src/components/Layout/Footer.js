341680b6113025653a9f7dc89a4e248027034660
jaweria-dev <jaweriaahmed25@gmail.com>
2024-07-20 05:19:27 -0700

    fix client error

---
+++ client1/src/components/Layout/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="bg-dark text-light p-3">
+        <h4 className="text-center">
+          All right reserved &copy; Jaweria Ahmed Siddiqui
+        </h4>
+        <p className="text-center mt-3">
+          <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
+          <Link to="/policy">Privacy Policy</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ client1/src/pages/Auth/Register.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import Layout from "../../components/Layout/Layout";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import "../../style/AuthStyles.css";
+import axios from "axios";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [answer, setAnswer] = useState("");
+  const Navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        "http://localhost:8080/api/v1/auth/register",
+        {
+          name,
+          email,
+          password,
+          phone,
+          address,
+          answer,
+        }
+      );
+      if (res && res.data.success) {
+        toast.success(res.data && res.data.message);
+        Navigate("/login");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <Layout title={"Register - Ecommerce App"}>
+      <div className="form-container ">
+        <h1>Register Page</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your Name"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your Email"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter your Password"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your Phone"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your Address"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="What is Your Favourite sports?"
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Register;
